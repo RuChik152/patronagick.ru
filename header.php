f7354e7c72adb5ec4fba9add0c816dee652ff20b
RuChik152 <57956402+RuChik152@users.noreply.github.com>
2020-01-19 22:44:02 +0300

    index.php

--- header.php
+++ header.php
@@ -1,3 +1,14 @@
+<?php
+
+$arr_page = [
+  ['Помощь маме'],
+  ['Рубрики', 'Первая пища малыша', 'Уход за новорожденным', 'Душевный покой мамы', 'ох, уж этот животик', 'почему малыш желтый', 'прививки'],
+  ['Задать вопрос'],
+  ['о проекте']
+];
+
+
+?>
 <!DOCTYPE html>
 <html lang="ru"></html>
 <head>
@@ -13,7 +24,23 @@
   <header>
     <div class="main__menu"><a href="index.html">          <img src="./images/logo.svg" alt="logo"/></a><img class="mobile_banner" src="./images/banner.png" alt="baner"/>
       <ul class="main__menu__item clearfix down_menu--horz">
-        <li><a class="mobile_font_size" href="help_mom.html">ПОМОЩЬ МАМЕ</a></li>
+        <?php         
+          if (isset($arr_page)) 
+          {
+            for ($i=0; $i < count($arr_page); ++$i) {
+                echo "<li><a class=\"mobile_font_size\" href=\"index.php?id=$i\">{$arr_page[$i][0]}</a>";
+                    if (count($arr_page[$i]) > 1) {
+                        echo "<ul class=\"down__menu\">";
+                            for ($a=1; $a < count($arr_page[$i]); ++$a) { 
+                                echo "<li><a href=\"index.php?head=$a\">{$arr_page[$i][$a]}</a></li>";
+                            }
+                        echo "</ul>";
+                    }
+                echo "</li>";
+            }
+          }  
+        ?>
+        <!-- <li><a class="mobile_font_size" href="help_mom.html">ПОМОЩЬ МАМЕ</a></li>
         <li><a class="mobile_font_size" href="#">РУБРИКИ</a>
           <ul class="down__menu">
             <li><a href="topic.html">Первая пища малыша</a></li>
@@ -25,7 +52,7 @@
           </ul>
         </li>
         <li><a class="mobile_font_size" href="feedback.html">ЗАДАТЬ ВОПРОС</a></li>
-        <li><a class="mobile_font_size about" href="about.html">О проекте</a></li>
+        <li><a class="mobile_font_size about" href="about.html">О проекте</a></li> -->
       </ul>
     </div>
   </header>
--- index.php
+++ index.php
@@ -5,7 +5,23 @@
 <?php include "header.php"; ?>
 <!-- end header.php -->
 
-
+<!-- start content -->
+<div class="wrap">
+    <div class="content">
+        <section>
+            <?php
+                if (!isset($_GET['p_id'])) {
+                    echo "<div class=\"section__banner\"><a href=\"about.html\">
+                    <div class=\"button\"> 
+                      <p>О ПРОЕКТЕ</p>
+                    </div></a></div>";
+                }
+            ?>
+            
+        </section>
+    </div>
+</div>
+<!-- end content -->
 
 <!-- start footer.php -->
 <?php include "footer.php"; ?>
