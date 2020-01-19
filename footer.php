4f855f876ba41f72e7670178610d7b6611accbca
RuChik152 <57956402+RuChik152@users.noreply.github.com>
2020-01-19 20:40:00 +0300

    start work on php

---
+++ footer.php
@@ -0,0 +1,9 @@
+  <!--footer-->
+  <footer>
+    <div class="footer_p">
+      <p>made <a href="#">nikita chernov</a></p>
+      <p>copyrite <a href="#">potronagick.ru</a></p>
+    </div>
+  </footer>
+  <!--end footer-->
+</body>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<!-- start header.php -->
+<?php include "header.php"; ?>
+<!-- end header.php -->
+
+
+
+<!-- start footer.php -->
+<?php include "footer.php"; ?>
+<!-- end footer.php -->
