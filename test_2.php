ba751dcbbfdae11da07cb5cef7e73daf9c1cec12
RuChik152 <57956402+RuChik152@users.noreply.github.com>
2020-01-31 23:47:01 +0300

    MySQL

--- test_2.php
+++ test_2.php
@@ -8,14 +8,18 @@ print_r($_GET);
 
     while ($row = mysqli_fetch_assoc($query)) {
     
-        $arr_article[] = array_values($row);
-    
+        $row_str = $row['menu_name'];
+        $arr_str[] = $row['menu_name'];
+
     }
 
-    echo "<pre>";
-    // echo $arr_article;
-    print_r($arr_article);
-    echo "</pre>";
+    print_r($arr_str);
+   
+
+    // echo "<pre>";
+    // // echo $arr_article;
+    // print_r($arr_article);
+    // echo "</pre>";
 
     // if ($arr_article[1][0] == true) {
     //     echo "существует";
@@ -23,4 +27,6 @@ print_r($_GET);
     //     echo "не существует";
     // }
 
+    // $arr_article_str = implode("|", $arr_article);
+    // echo $arr_article_str;
 ?>
